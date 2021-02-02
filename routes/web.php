906cbc016a6208f19e49104f8bea986f7daef912
vminv <aminaseitkulova@gmail.com>
2021-02-02 19:53:28 +0500

    route

--- routes/web.php
+++ routes/web.php
@@ -354,8 +354,6 @@ Route::get('/cabinet', 'CabinetController@index')->name('cabinet');
 Route::get('/poisk/bakalavriat/', 'PoiskController@index')->name('bakalavriat');
 Route::get('/poisk/magistratura/', 'PoiskController@index')->name('magistratura');
 Route::get('/poisk/doktorantura/', 'PoiskController@index')->name('doktorantura');
-Route::get('/calculator', 'СalculatorController@index')->name('calculator');
-Route::post('/calculator', 'СalculatorController@postResult');
 Route::get('/rating/', 'RatingController@index')->name('rating');
 Route::get('/list/', 'ListController@index')->name('list');
 Route::get('/fmain/{degree_id}/{direction_id}/{city_id}/{query}', 'ListController@getFmain')->name('list');
